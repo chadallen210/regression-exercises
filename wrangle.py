2f4c0ae64b7790a85b7307d586a988f126ac6918
Chad Allen <chad.allen.210@gmail.com>
2021-06-07 15:22:25 -0500

    updates

--- wrangle.py
+++ wrangle.py
@@ -13,7 +13,53 @@ def get_db_url(db, username=username, host=host, password=password):
     
     return f'mysql+pymysql://{username}:{password}@{host}/{db}'
 
-##### TELCO FUNCTIONS #####
+def wrangle_grades():
+    '''
+    Read student_grades csv file into a pandas DataFrame,
+    drop student_id column, replace whitespaces with NaN values,
+    drop any rows with Null values, convert all columns to int64,
+    return cleaned student grades DataFrame.
+    '''
+    # Acquire data from csv file.
+    grades = pd.read_csv('student_grades.csv')
+
+    # Replace white space values with NaN values.
+    grades = grades.replace(r'^\s*$', np.nan, regex=True)
+
+    # Drop all rows with NaN values.
+    df = grades.dropna()
+
+    # Convert all columns to int64 data types.
+    df = df.astype('int')
+
+    return df
+    
+    
+# Generic splitting function for continuous target.
+
+def split_continuous(df):
+    '''
+    Takes in a df
+    Returns train, validate, and test DataFrames
+    '''
+    # Create train_validate and test datasets
+    train_validate, test = train_test_split(df, 
+                                        test_size=.2, 
+                                        random_state=123)
+    # Create train and validate datsets
+    train, validate = train_test_split(train_validate, 
+                                   test_size=.3, 
+                                   random_state=123)
+
+    # Take a look at your split datasets
+
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')
+    return train, validate, test
+
+    
+    ##### TELCO FUNCTIONS #####
 def new_telco_data():
     '''
     gets telco_churn information from CodeUp db and creates a dataframe
@@ -125,3 +171,16 @@ def wrangle_zillow():
     df = df.dropna()
         
     return df
+
+def split_zillow(df):
+    '''
+    this function takes in the zillow data 
+    '''
+    
+    # split df into 20% test, 80% train_validate
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=1234)
+    
+    # split train_validate into 30% validate, 70% train
+    train, validate = train_test_split(train_validate, test_size=0.3, random_state=1234)
+    
+    return train, validate, test
